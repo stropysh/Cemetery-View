ada091a73f8d833b48dd303543d763e69675762f
stropysh <stropysh@gmail.com>
2011-07-21 02:06:37 +0400

    1

---
+++ TEST/Test_Project.dpr
@@ -0,0 +1,14 @@
+program Test_Project;
+
+uses
+  Forms,
+  Test1 in 'Test1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
